52f1e951996334f11061b84b397f1616ffd8cc26
KeatonBuddy <47367626+KeatonBuddy@users.noreply.github.com>
2020-01-20 23:28:18 -0700

    Add files via upload

--- Compare.py
+++ Compare.py
@@ -6,18 +6,18 @@ import math
 startTime = time.perf_counter()
 
 def Compare(s1, s2):
-    difference = abs(s1-s2)
+    difference = abs(float(s1)-float(s2))
     print(difference)
 
 
 
 with open('test.txt') as csv_file:
 
-    csv_reader = csv.DictReader(csv_file)
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_number = 0
     for row in csv_reader:
-        id1 = row["ID"]
-        print(id1)
-              
+        Compare(row[1],row[2])
+            
 
 
 
