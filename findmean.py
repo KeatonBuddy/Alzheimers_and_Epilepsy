6591c12f053c517fdffc22eeec59d640cdca7144
abisubu18 <47370590+abisubu18@users.noreply.github.com>
2020-01-22 15:45:12 -0700

    Update findmean.py
    now it puts the means into two different files

--- findmean.py
+++ findmean.py
@@ -2,10 +2,6 @@ import csv
 
 nodem_file = input("Enter file name: ")
 
-#nodem_file = "finalsheet_nodem.csv"
-
-#fields = []
-#rows = []
 
 with open(nodem_file) as csvfile:
 
@@ -15,34 +11,41 @@ with open(nodem_file) as csvfile:
     sum_of_gene = 0
 
     if nodem_file == "finalsheet_nodem.csv":
-        for row in csv_reader:
 
-            if line_count <= 1:
-                print(f'Column names are {",".join(row)}')
-                line_count += 1
-            else:
-                for i in range (2,53):
-                    sum_of_gene += float(row[i])
-                #print(f'\t{row[0]} has expressions {row[i]}.')
-                mean_of_gene = sum_of_gene/50
-                print(f'\t{row[0]} And the mean is'+str(mean_of_gene))
-                sum_of_gene = 0
-                line_count += 1
-    
-        print(f'Processed{line_count} lines.')
+        with open('mean_n0dem_file.csv', 'w', newline='') as file:
+
+               for row in csv_reader:
+                    if line_count <= 1:
+                        #print(f'Column names are {",".join(row)}')
+                        line_count += 1
+                    else:
+                         for i in range (2,53):
+                              sum_of_gene += float(row[i])
+                         #print(f'\t{row[0]} has expressions {row[i]}.')
+                         mean_of_gene = sum_of_gene/50
+                         print(f'\t{row[0]} And the mean is'+str(mean_of_gene))
+                         writer = csv.writer(file)
+                         writer.writerow([mean_of_gene])
+                         sum_of_gene = 0
+                         line_count += 1
+               print(f'Processed{line_count} lines.')
 
     elif nodem_file == "finalsheet_demalz.csv":
-        for row in csv_reader:
 
-            if line_count <= 1:
-                print(f'Column names are {",".join(row)}')
-                line_count += 1
-            else:
-                for i in range (2,37):
-                    sum_of_gene += float(row[i])
-                #print(f'\t{row[0]} has expressions {row[i]}.')
-                mean_of_gene = sum_of_gene/50
-                print(f'\t{row[0]} And the mean is'+str(mean_of_gene))
-                sum_of_gene = 0
-                line_count += 1
+        with open('mean_alz_file.csv','w', newline='') as file:
+
+            for row in csv_reader:
+                if line_count <= 1:
+                    #print(f'Column names are {",".join(row)}')
+                    line_count += 1
+                else:
+                    for i in range (2,37):
+                        sum_of_gene += float(row[i])
+                    #print(f'\t{row[0]} has expressions {row[i]}.')
+                    mean_of_gene = sum_of_gene/35
+                    print(f'\t{row[0]} And the mean is'+str(mean_of_gene))
+                    writer = csv.writer(file)
+                    writer.writerow([mean_of_gene])
+                    sum_of_gene = 0
+                    line_count += 1
     
