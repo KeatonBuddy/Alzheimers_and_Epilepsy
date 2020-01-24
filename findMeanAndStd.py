f51808ca5f979c1d1d3ff62d44dc24ba269ce95d
abisubu18 <47370590+abisubu18@users.noreply.github.com>
2020-01-23 20:33:32 -0700

    Update findMeanAndStd.py

--- findMeanAndStd.py
+++ findMeanAndStd.py
@@ -1,58 +1,39 @@
 import csv
 import statistics
+import pandas
 
 nodem_file = input("Enter file name: ")
+output_file = input("Enter file output name:")
 s = ()
 
 with open(nodem_file) as csvfile:
 
+    pandaDemFile = pandas.read_csv(nodem_file)
+    rows = pandaDemFile.shape[0]
+    columns = pandaDemFile.shape[1]
 
     csv_reader = csv.reader(csvfile,delimiter = ',')
     line_count = 0
     sum_of_gene = 0
 
-    if nodem_file == "finalsheet_nodem.csv":
+    with open(f"{output_file}", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Mean","STD"])
 
-        with open('mean_n0dem_file.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["Mean","STD"])
-
-            for row in csv_reader:
-                if line_count <= 1:
-                    line_count += 1
-                else:
-                        for i in range (2,53):
-                            sum_of_gene += float(row[i])
-                            s = s + (float(row[i]),)
-                        hello = statistics.stdev(s)
-                        print("Standard Deviation of sample is % s " % (hello)) 
-                        mean_of_gene = sum_of_gene/50
-                        print(f'\t{row[0]} And the mean is'+str(mean_of_gene))
-                        writer = csv.writer(file)
-                        writer.writerow([mean_of_gene, hello])
-                        s = ()
-                        sum_of_gene = 0
-                        line_count += 1
-            print(f'Processed{line_count} lines.')
-
-    elif nodem_file == "finalsheet_demalz.csv":
-
-        with open('mean_alz_file.csv','w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["Mean","STD"])
-            for row in csv_reader:
-                if line_count <= 1:
-                    line_count += 1
-                else:
-                    for i in range (2,37):
+        for row in csv_reader:
+            if line_count <= 1:
+                line_count += 1
+            else:
+                    for i in range (2,int(columns)):
                         sum_of_gene += float(row[i])
                         s = s + (float(row[i]),)
                     hello = statistics.stdev(s)
-                    print("Standard Deviation of sample is % s " % (hello))
-                    mean_of_gene = sum_of_gene/35
+                    print("Standard Deviation of sample is % s " % (hello)) 
+                    mean_of_gene = sum_of_gene/(columns-2)
                     print(f'\t{row[0]} And the mean is'+str(mean_of_gene))
+
                     writer.writerow([mean_of_gene, hello])
                     s = ()
                     sum_of_gene = 0
                     line_count += 1
-    
+        print(f'Processed{line_count} lines.')
